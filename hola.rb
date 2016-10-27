4a561b9190f538238dc13e130707fa4ed24858eb
Jesus Martin Castro <jesmartin82@gmail.com>
2016-10-27 14:10:54 +0200

    Contribuyendo a David

--- hola.rb
+++ hola.rb
@@ -2,4 +2,4 @@ puts"Dime tu nombre"
 nombre = gets.chomp
 puts "Hola " + nombre + " encantado de conocerte!"
 puts "Aquí estamos aprendiendo ruby"
-
+puts "Procurando contribuir en el repositorio de David Leon"
